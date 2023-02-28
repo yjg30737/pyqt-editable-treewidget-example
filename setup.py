29fc30539795008a8baf7c4c06ae2d0401d2a516
Jung Gyu Yoon <yjg30737@gmail.com>
2023-02-28 13:53:01 +0900

    Bump ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-editable-treewidget-example',
-    version='0.0.11',
+    version='0.0.111',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
